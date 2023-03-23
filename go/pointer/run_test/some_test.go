6f500f256b225804204cb1ebac4ff95988d9a385
cenaras <mathias_rud@hotmail.com>
2023-03-23 10:42:27 +0100

    Commit for test

--- go/pointer/run_test/some_test.go
+++ go/pointer/run_test/some_test.go
@@ -7,7 +7,7 @@ import (
 
 func Test(t *testing.T) {
 	// Don't assert @pointsto(t) since its label contains a fragile line number.
-	run_engine("../../testdata/arrays.go", nil)
+	run_engine("from_goat.go", nil)
 	t.Fail()
 }
 
