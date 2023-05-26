4e794da93b263a399e5aef55045299afead3bc66
cenaras <mathias_rud@hotmail.com>
2023-05-26 11:54:33 +0200

    interface nil skip

--- go/pointer/solve.go
+++ go/pointer/solve.go
@@ -298,7 +298,9 @@ func (c *offsetAddrConstraint) solve(a *analysis, delta *nodeset) {
 func (c *typeFilterConstraint) solve(a *analysis, delta *nodeset) {
 	for _, x := range delta.AppendTo(a.deltaSpace) {
 		ifaceObj := nodeid(x)
-
+		if (ifaceObj == a.nilNode) {
+			continue
+		}
 		tDyn, _, indirect := a.taggedValue(ifaceObj)
 		if indirect {
 			// TODO(adonovan): we'll need to implement this
@@ -321,6 +323,9 @@ func (c *untagConstraint) solve(a *analysis, delta *nodeset) {
 	}
 	for _, x := range delta.AppendTo(a.deltaSpace) {
 		ifaceObj := nodeid(x)
+		if (ifaceObj == a.nilNode) {
+			continue
+		}
 		tDyn, v, indirect := a.taggedValue(ifaceObj)
 		if indirect {
 			// TODO(adonovan): we'll need to implement this
@@ -343,7 +348,9 @@ func (c *untagConstraint) solve(a *analysis, delta *nodeset) {
 func (c *invokeConstraint) solve(a *analysis, delta *nodeset) {
 	for _, x := range delta.AppendTo(a.deltaSpace) {
 		ifaceObj := nodeid(x)
-
+		if (ifaceObj == a.nilNode) {
+			continue
+		}
 		tDyn, v, indirect := a.taggedValue(ifaceObj)
 		if indirect {
 			// TODO(adonovan): we may need to implement this if
